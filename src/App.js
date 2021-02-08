04f096d5b2cb42541d6b67ba5ff2bc93624c2281
Elyse Wells <cewells89@gmail.com>
2021-02-07 21:48:49 -0500

    added navbar router links. added hard coded content to test navigation. importing of navbar on all pages and routing is working.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { HashRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Experience from "./Pages/Experience/Experience";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router basename="/">
+        <NavBar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Portfolio" component={Portfolio} /> 
+        <Route exact path="/Experience" component={Experience} /> 
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="nav">
+                <a className="nav-link">
+                    <Link to="/" className={window.location.pathname === "/" ||window.location.pathname === "/About"? "nav-link active": "nav-link"}>About</Link> 
+                </a>
+                <a className="nav-link">
+                    <Link to="/Portfolio" className={window.location.pathname === "/Portfolio"? "nav-link active": "nav-link"}>Portfolio</Link> 
+                </a>
+                <a className="nav-link">
+                    <Link to="/Experience" className={window.location.pathname === "/Experience"? "nav-link active": "nav-link"}>Experience</Link> 
+                </a>
+                <a className="nav-link" >
+                    <Link to="/Contact" className={window.location.pathname === "/Contact"? "nav-link active": "nav-link"}>Contact</Link> 
+                </a>
+            </nav>            
+        </div>
+    );
+};
+
+export default NavBar;
